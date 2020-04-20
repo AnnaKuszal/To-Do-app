20928be9016dc67c6091877eb176040af075ae74
Anna Kuszal <anna.kuszal@o2.pl>
2020-04-20 19:37:31 +0200

    Add listeners to server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
+
 app.use(express.static(path.join(__dirname + '/client')));
 
 app.get('*', (req, res) => {
@@ -19,3 +21,23 @@ app.use((req, res) => {
 });
 
 const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! It\'s id – ' + socket.id);
+
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    console.log('New task added', task);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (index) => {
+    console.log('Task with id ' + index + ' removed');
+    socket.broadcast.emit('removeTask', index);
+    tasks.splice(index, 1);
+  });
+
+
+});
