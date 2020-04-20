6352d7d083ac45ab6eec0b1ab84370b66a4c2081
Anna Kuszal <anna.kuszal@o2.pl>
2020-04-20 18:38:02 +0200

    Create React app structure

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname + '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
